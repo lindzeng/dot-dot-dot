12e0ae4cc78844a1320a026c100c0cb6d8ac58a7
lindzeng <lindyzeng.lz@gmail.com>
2017-05-10 01:09:09 -0400

    Add moving dots

--- dots.js
+++ dots.js
@@ -1,29 +1,70 @@
 var type = "WebGL"
 
-if(!PIXI.utils.isWebGLSupported()){
+if(!PIXI.utils.isWebGLSupported()) {
     type = "canvas";
 }
 
-PIXI.utils.sayHello(type);
-
 var width = 512;
 var height = 512;
+var numDots = 20;
 
-//Create the renderer
-var renderer = PIXI.autoDetectRenderer(width, height);
+var Container = PIXI.Container,
+    autoDetectRenderer = PIXI.autoDetectRenderer,
+    loader = PIXI.loader,
+    resources = PIXI.loader.resources,
+    TextureCache = PIXI.utils.TextureCache,
+    Sprite = PIXI.Sprite;
+    Graphics = PIXI.Graphics;
 
-//Add the canvas to the HTML document
+//Create a Pixi stage and renderer and add the renderer.view to the DOM
+var stage = new Container(),
+    renderer = autoDetectRenderer(width, height, 
+        {antialias: false, transparent: false, resolution: 1});
+renderer.view.style.position = "absolute";
+renderer.view.style.display = "block";
+renderer.autoResize = true;
+renderer.resize(window.innerWidth, window.innerHeight);
+renderer.backgroundColor = 0xffffff;
 document.body.appendChild(renderer.view);
 
-//Create a container object called the `stage`
-var stage = new PIXI.Container();
+var dots = [];
+setup();
 
-//Tell the `renderer` to `render` the `stage`
-renderer.render(stage);
+function setup() {
+  console.log("setup"); 
+  //Create the sprites
+    // cat = new Sprite(resources["images/cat.png"].texture),
+    //     blob = new Sprite(resources["images/blob.png"].texture),
+    //     explorer = new Sprite(resources["images/explorer.png"].texture);
 
-// Detect whether to use canvas drawing API or WebGL
-// Third options arguments {} is optional
-renderer = PIXI.autoDetectRenderer(
-    width, height,
-    {antialias: false, transparent: false, resolution: 1}
-);
+    for (var i = 0; i < numDots; i++) {
+        var d = new Graphics();
+        d.beginFill(0x9966FF);
+        d.drawCircle(0, 0, 15);
+        d.endFill();
+        d.x = Math.random() * window.innerWidth;
+        d.y = Math.random() * window.innerHeight;
+        d.vx = Math.random() * 1;
+        d.vy = Math.random() * 1;
+        dots.push(d);
+        stage.addChild(d);
+    }
+
+    //Start the game loop
+    gameLoop();
+}
+
+function gameLoop(){
+  //Loop this function 60 times per second
+  requestAnimationFrame(gameLoop);
+  
+  // Move the dots
+  for (var i = 0; i < numDots; i++) {
+    dots[i].x += dots[i].vx;
+    dots[i].y += dots[i].vy;
+  }
+  
+  //Render the stage
+  renderer.render(stage);
+}
+
